a5f95eb9e49603e0bd15b99d63bd946148a08e85
Vinoth Kumar Shanmugam <vinoth.shanmugam@delhivery.com>
2024-06-13 20:51:15 -0400

    add weather api lookup

--- internal/service/weather.go
+++ internal/service/weather.go
@@ -1,15 +1,66 @@
 package service
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"weathe-service/internal/entity"
+)
 
 type WeatherService struct {
+	apiKey string
 }
 
 func NewWeatherService() *WeatherService {
-	return &WeatherService{}
+	key := os.Getenv("WEATHER_SERVICE_API_KEY")
+	if key == "" {
+		log.Fatal("WEATHER_SERVICE_API_KEY is not set")
+		panic("WEATHER_SERVICE_API_KEY is not set")
+	}
+	return &WeatherService{
+		apiKey: key,
+	}
 }
 
 func (s *WeatherService) GetWeather(ctx context.Context, lat, lon float32) string {
-	// call weather api here
-	return "Sunny"
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s",
+		strconv.FormatFloat(float64(lat), 'f', 6, 32),
+		strconv.FormatFloat(float64(lon), 'f', 6, 32),
+		s.apiKey,
+	)
+
+	httpClient := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("failed to fetch weather data: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var weatherResponse entity.WeatherResponse
+	err = json.Unmarshal(body, &weatherResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return weatherResponse.Weather[0].Main
 }
